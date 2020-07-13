3585ee05bc40863cb105710996d98bace37c039e
seungh0 <will.seungho@gmail.com>
2020-07-13 19:00:58 +0900

    Initial commit

---
+++ src/main/java/me/will/demobootwebmvc/DemoBootWebMvcApplication.java
@@ -0,0 +1,13 @@
+package me.will.demobootwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBootWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBootWebMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/will/demobootwebmvc/DemoBootWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.will.demobootwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBootWebMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
