9d99d6b1181ac6cbaa74ffd198db268f248474e6
seungh0 <will.seungho@gmail.com>
2020-07-14 10:57:03 +0900

    [Resource-handler] implement Resource Handler

--- src/test/java/me/will/demobootwebmvc/controller/SampleControllerTest.java
+++ src/test/java/me/will/demobootwebmvc/controller/SampleControllerTest.java
@@ -3,16 +3,20 @@ package me.will.demobootwebmvc.controller;
 
 import me.will.demobootwebmvc.domain.Man;
 import me.will.demobootwebmvc.domain.ManRepository;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @SpringBootTest
@@ -54,6 +58,23 @@ class SampleControllerTest {
 				.param("id", man.getId().toString()))
 				.andDo(print())
 				.andExpect(content().string("hello will"));
+	}
+
+	@Test
+	void helloStatic() throws Exception {
+		this.mockMvc.perform(get("/index.html"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string(Matchers.containsString("Hello index")));
+	}
+
+	@Test
+	void helloMobileStatic() throws Exception {
+		this.mockMvc.perform(get("/mobile/index.html"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string(Matchers.containsString("Hello Mobile")))
+				.andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
 	}
 
 }
