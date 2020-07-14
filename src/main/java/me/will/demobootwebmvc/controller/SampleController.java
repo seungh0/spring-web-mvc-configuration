182325117ac6c583ede501741272ec9a66e1cd17
will <will.seungho@gmail.com>
2020-07-14 11:51:11 +0900

    Feature/http message converter (#5)
    * [Http-Message-Converter] Message Converter
    
    * [Http-Message-Converter] Message Converter (Json)

--- src/main/java/me/will/demobootwebmvc/controller/SampleController.java
+++ src/main/java/me/will/demobootwebmvc/controller/SampleController.java
@@ -4,6 +4,7 @@ import me.will.demobootwebmvc.domain.Man;
 import me.will.demobootwebmvc.domain.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,16 @@ public class SampleController {
 	@GetMapping("/hello")
 	public String helloId(@RequestParam("id") Man man) {
 		return "hello " + man.getName();
+	}
+
+	@GetMapping("/message")
+	public String message(@RequestBody String body) {
+		return body;
+	}
+
+	@GetMapping("/jsonMessage")
+	public Man jsonMessage(@RequestBody Man man) { //원래는 Dto로 변환 해야함 ㅠ.
+		return man;
 	}
 
 }
