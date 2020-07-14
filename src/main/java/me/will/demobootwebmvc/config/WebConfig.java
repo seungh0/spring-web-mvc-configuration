e60a9b881c329bc5ec6466f4f967f69f82955d01
will <will.seungho@gmail.com>
2020-07-14 10:11:55 +0900

    [Handler Interceptor] implement handler interceptor (#3)

--- src/main/java/me/will/demobootwebmvc/config/WebConfig.java
+++ src/main/java/me/will/demobootwebmvc/config/WebConfig.java
@@ -1,7 +1,10 @@
 package me.will.demobootwebmvc.config;
 
+import me.will.demobootwebmvc.config.interceptor.AnotherInterceptor;
+import me.will.demobootwebmvc.config.interceptor.GreetingInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +18,14 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addFormatters(FormatterRegistry registry) {
 		registry.addFormatter(new PersonFormatter());
 	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new GreetingInterceptor());
+		registry.addInterceptor(new AnotherInterceptor())
+				.addPathPatterns("/hello/*") // 특정 Path에만 적용하고 싶을 경우
+				.order(1); // 우선순위 커스텀하고 싶을때 (작을수록 높음)
+	}
+	// preHandle1 -> preHandle2 -> postHandle2 -> postHandle1 -> afterCompletion2 -> afterCompletion1
 
 }
